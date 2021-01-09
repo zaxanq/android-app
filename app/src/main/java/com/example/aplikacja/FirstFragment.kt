7085cad8fa762c9d78918bb512cde99673fbacbc
kkosmowski <krzysztof@kosmowski.ct8.pl>
2021-01-09 14:20:48 +0100

    Task #4 - [Logika] Rejestracja krok 1/2

--- app/src/main/java/com/example/aplikacja/FirstFragment.kt
+++ app/src/main/java/com/example/aplikacja/FirstFragment.kt
@@ -1,17 +1,24 @@
 package com.example.aplikacja
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private var nameInput: EditText? = null;
+    private var passwordInput: EditText? = null;
+    private var repeatPasswordInput: EditText? = null;
+    private var continueButton: Button? = null;
+    private var warningItem: TextView? = null;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +30,52 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        nameInput = requireView().findViewById(R.id.nameInput);
+        passwordInput = requireView().findViewById(R.id.passwordInput)
+        repeatPasswordInput = requireView().findViewById(R.id.repeatPasswordInput)
+        continueButton = requireView().findViewById(R.id.continueButton)
+        warningItem = requireView().findViewById(R.id.firstScreen_warning)
 
-        view.findViewById<Button>(R.id.continueButton).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        continueButton?.setOnClickListener {
+            if (
+                this.checkIfInputLengthIsValid(
+                    nameInput,
+                    3,
+                    R.string.warning_nameTooShort
+                ) &&
+                this.checkIfInputLengthIsValid(
+                    passwordInput,
+                    5,
+                    R.string.warning_passwordTooShort
+                ) &&
+                this.checkIfPasswordsAreIdentical()
+            ) {
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
+        }
+    }
+
+    private fun checkIfInputLengthIsValid(
+        input: EditText?,
+        validLength: Int,
+        warningId: Int
+    ): Boolean {
+        return if (input?.text.toString().length >= validLength) {
+            warningItem?.text = "";
+            true;
+        } else {
+            warningItem?.text = resources.getString(warningId);
+            false;
+        }
+    }
+
+    private fun checkIfPasswordsAreIdentical(): Boolean {
+        return if (passwordInput?.text.toString() == repeatPasswordInput?.text.toString()) {
+            warningItem?.text = "";
+            true;
+        } else {
+            warningItem?.text = resources.getString(R.string.warning_passwordsAreNotIdentical);
+            false;
         }
     }
 }
