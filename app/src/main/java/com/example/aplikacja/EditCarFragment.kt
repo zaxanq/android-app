bbfb3840147f9207e74bf67ba40a63dca2d73ce5
kkosmowski <krzysztof@kosmowski.ct8.pl>
2021-01-12 22:36:34 +0100

    poprawki

--- app/src/main/java/com/example/aplikacja/EditCarFragment.kt
+++ app/src/main/java/com/example/aplikacja/EditCarFragment.kt
@@ -6,12 +6,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import java.io.File
 import java.io.FileWriter
-import java.lang.Exception
 
 
 /**
@@ -28,8 +29,8 @@ class EditCarFragment : Fragment() {
 
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.editcar_screen, container, false)
@@ -39,15 +40,16 @@ class EditCarFragment : Fragment() {
 
     }
 
-    private fun loadData(){
-        val shearedPreferences = requireActivity().getSharedPreferences("sheredPrefs", Context.MODE_PRIVATE)
+    private fun loadData() {
+        val shearedPreferences =
+            requireActivity().getSharedPreferences("sheredPrefs", Context.MODE_PRIVATE)
         val savedName = shearedPreferences?.getString("NAME_KEY", null)
 
         //addFirstCarTitleText?.text = savedName
         //Toast.makeText(requireContext(), savedName, Toast.LENGTH_SHORT).show()
     }
 
-    private fun CreateFile(str:String){
+    private fun CreateFile(str: String) {
 
         var fo = FileWriter("test.txt")
         fo.write("test")
@@ -76,10 +78,7 @@ class EditCarFragment : Fragment() {
                 builder.setPositiveButton("OK") { dialog, which ->
                     dialog.dismiss()
                 }
-            }
-
-
-            else if(selectBrandSpinnerText.trim().length<=1) {
+            } else if (selectBrandSpinnerText.trim().length <= 1) {
                 val builder: AlertDialog.Builder = AlertDialog.Builder(this.context)
                 builder.setTitle("Error")
                 builder.setMessage("Wybierz markę pojazdu!")
@@ -88,9 +87,7 @@ class EditCarFragment : Fragment() {
                 builder.setPositiveButton("OK") { dialog, which ->
                     dialog.dismiss()
                 }
-            }
-
-            else if(selectModelSpinnerText.trim().length<=1) {
+            } else if (selectModelSpinnerText.trim().length <= 1) {
                 val builder: AlertDialog.Builder = AlertDialog.Builder(this.context)
                 builder.setTitle("Error")
                 builder.setMessage("Wybierz model pojazdu!")
@@ -99,8 +96,7 @@ class EditCarFragment : Fragment() {
                 builder.setPositiveButton("OK") { dialog, which ->
                     dialog.dismiss()
                 }
-            }
-            else{
+            } else {
                 CreateFile(addLicensePlateInputText)
                 /*( val fileName = "data.txt"
 
@@ -121,29 +117,8 @@ class EditCarFragment : Fragment() {
                 //File.createNewFile()
                 //File("Cars.txt").writeText(selectTypeSpinnerText + ';' + selectBrandSpinnerText + ';' + selectModelSpinnerText + ';' + addLicensePlateInputText + ';' + addMeterStatusInputText)
                 //File("$addLicensePlateInputText.txt").writeText(selectTypeSpinnerText + ';' + selectBrandSpinnerText + ';' + selectModelSpinnerText + ';' + addLicensePlateInputText + ';' + addMeterStatusInputText)
-                findNavController().navigate(R.id.action_SecondFragment_to_HomeFragment)
+                findNavController().navigate(R.id.action_Second_to_EkranGlowny)
             }
         }
-
-
-
-        fun loadSpinnerResources(spinner: Spinner, resources: Int) {
-            // load brands list
-            // get array of brands from resource (vehicleBrands.xml)
-            ArrayAdapter.createFromResource(
-                    requireActivity(), // ?
-                    resources, // source
-                    android.R.layout.simple_spinner_item // map items to simple_spinner_item
-            ).also { adapter -> // then
-                // load simple_spinner_items as items of dropdown
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinner.adapter = adapter // then load whole "adapter" into brand spinner
-            }
-        }
-
-        loadSpinnerResources(view.findViewById(R.id.selectTypeSpinner), R.array.vehicleTypesArray)
-        loadSpinnerResources(view.findViewById(R.id.selectBrandSpinner), R.array.vehicleBrandsArray)
-        loadSpinnerResources(view.findViewById(R.id.selectModelSpinner), R.array.vehicleModelsArray)
-
     }
 }
